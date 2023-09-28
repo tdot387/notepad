1662c6af09924fa1bc529e4fdd09c740b31bfd91
Thomas Wagner <t-wagner@web.de>
2023-09-28 17:19:49 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'notepad-bae31',
+    appId: '1:516265915411:web:7c54e9fc4b3bb4654abbe6',
+    storageBucket: 'notepad-bae31.appspot.com',
+    apiKey: 'AIzaSyD0hhk3ynzWx_O-ll0QZDOH3pXbkmRwzNI',
+    authDomain: 'notepad-bae31.firebaseapp.com',
+    messagingSenderId: '516265915411',
+  },
+};
