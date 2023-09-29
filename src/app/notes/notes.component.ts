a3fa00766d3ebefec6bb1d3e8272a033fe66a3b3
Thomas Wagner <t-wagner@web.de>
2023-09-29 16:36:46 +0200

    created cards

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-notes',
@@ -8,11 +9,27 @@ import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 })
 export class NotesComponent {
 
-  constructor(private firestore: Firestore) {}
+  noteData!: Observable<any>;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
 
   uploadData(f: any) {
     const collectionInstance = collection(this.firestore, 'names');
     addDoc(collectionInstance, f.value);
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'names');
+    collectionData(collectionInstance)
+    .subscribe(val => {
+      console.log(val);
+      
+    })
+
+    this.noteData = collectionData(collectionInstance);
+    
   }
 
 }
